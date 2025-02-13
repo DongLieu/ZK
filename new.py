939c93d9ab28162f6979c102313256b69f588e44
Đông Liều <duongduydongtq@gmail.com>
2025-02-13 16:53:23 +0700

    new func

--- new.py
+++ new.py
@@ -1,3 +1,5 @@
+
+# Import module
 import random
 import groth16
 import numpy as np
@@ -6,17 +8,26 @@ import galois
 FP = groth16.FP
 p = groth16.p
 
+# f(x,y) = x^3 + x^2*y^2 +x*y^2-x^2+y^4 
 _x = FP(2)
 _y = FP(3)
-_witness = FP([1, _x, _y])
-
+_v1 = _x * _x
+_v2 = _y * _y
+out = _x**3 + _x**2*_y**2 + _x*_y**2 - _x**2 + _y**4
+_witness = FP([1, out, _x, _y, _v1, _v2])
 # ============================================== R1CS =============================================
 
-R = FP([[1, 1, 0]])
+R = FP([ [0, 0, 1, 0, 0, 0],
+         [0, 0, 0, 1, 0, 0],
+         [0, 0, 0, 0, 1, 1]])
 
-L = FP([[1, 0, 0]])
+L = FP([ [0, 0, 1, 0, 0, 0],
+         [0, 0, 0, 1, 0, 0],
+         [0, 0, 1, 0, 0, 1]])
 
-O = FP([[0, 0, 1]])
+O = FP([ [0, 0, 0, 0, 1, 0],
+         [0, 0, 0, 0, 0, 1],
+         [0, 1, 0, 0, 1, 0]])
 assert all(np.equal(np.matmul(L, _witness) * np.matmul(R, _witness), np.matmul(O, _witness))), "not equal"
 # # ============================================== QAP =============================================
 mtxs = [L, R, O]
@@ -47,7 +58,11 @@ T = galois.Poly([1, p-1], field=FP)
 for i in range(2, L.shape[0] + 1):
     T *= galois.Poly([1, p-i], field=FP)
 
+# # ============================================== groth16 =============================================
+qap = groth16.QAP(Lp, Rp, Op, T)
+_pk,vk = groth16.keygen(qap)
+proof = groth16.prove(_pk,_witness, qap)
 
-print(Lp)
-print(Rp)
-print(Op)
+w_public = groth16.get_witness_public(_pk, _witness)
+ok = groth16.verifier(vk,w_public, proof)
+print(ok)
